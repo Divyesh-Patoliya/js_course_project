08a8c070b39412e3e4ea2fb5778cf0d812923e21
Patoliya-Divyesh <patoliyadivyesh.pd@gmail.com>
2020-08-09 11:41:09 +0530

    Challange 4 : change the color of button

--- static/js/script.js
+++ static/js/script.js
@@ -94,3 +94,67 @@ function rpsFrontEnd(humanChoice,botChoice,messageForPrint) {
     document.getElementById('flex-box-rps-div').appendChild(botDiv);
 
 }
+// Challange : change the color of button
+
+// coleecting buttons from html 
+var all_buttons =document.getElementsByTagName('button');
+var copyAllButtons =[];
+
+for (let i=0;i<all_buttons.length;i++){
+    copyAllButtons.push(all_buttons[i].classList[1]);
+}
+
+//define buttons actions 
+function buttonColorChange(buttonThingy){
+    if(buttonThingy.value==='red'){
+        buttonsRed();
+    }else if (buttonThingy.value=== 'green'){
+        buttonsGreen();
+    }else if(buttonThingy.value === 'reset'){
+        buttonsReset();
+    }else if(buttonThingy.value === 'random'){
+        buttonsRandom();
+    }
+}
+
+// change all buttons in  red buttons 
+function buttonsRed(){
+    for (let i=0;i<all_buttons.length;i++){
+
+       all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+       all_buttons[i].classList.add('btn-danger');
+
+    }
+    
+}
+
+//Change button in to green
+function buttonsGreen(){
+    for (let i=0;i<all_buttons.length;i++){
+
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+ 
+     }
+
+}
+
+function buttonsReset(){
+    for (let i=0;i<all_buttons.length;i++){
+
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+ 
+     }
+}
+
+function buttonsRandom(){
+    var choises =['btn-primary','btn-primary','btn-danger','btn-warning']
+    for(let i=0; i<all_buttons.length;i++){
+
+        let randomNumber = Math.floor(Math.random()*4);
+
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(choises[randomNumber]);
+    }
+}
